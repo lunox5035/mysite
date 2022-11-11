355f6fe46fe4ff2df36ec988e0fd0d4ff60dbb42
GawnGyu Yeo <bbc02131@naver.com>
2022-11-11 12:09:57 +0900

    .

--- mysite02/src/main/java/com/bitacademy/mysite/controller/BoardController.java
+++ mysite02/src/main/java/com/bitacademy/mysite/controller/BoardController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class boardController extends HttpServlet {
+public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
