aefd69224c2ac9c129af2d369b24c3b57e611b6c
GawnGyu Yeo <bbc02131@naver.com>
2022-11-17 11:14:05 +0900

    예외처리 #exception

---
+++ mysite03/src/main/java/com/bitacademy/mysite/exception/ControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.bitacademy.mysite.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public String ExceptionHandler(Model model, Exception e) {
+		//로깅
+//		System.out.println(e);
+		StringWriter errors = new StringWriter();
+		e.printStackTrace(new PrintWriter(errors));
+		System.out.println(errors.toString());
+
+		//사과페이지(HTML 응답, 정상종료
+		model.addAttribute("exception",errors.toString());
+		return "error/exception";
+	}
+	
+}
